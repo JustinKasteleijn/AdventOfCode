f8da50238037c493a69155f6819f31366d71816a
JustinKasteleijn <48954608+JustinKasteleijn@users.noreply.github.com>
2023-06-02 21:28:50 +0200

    First attempt at day 3, here the learning really starts

--- Rust/src/main.rs
+++ Rust/src/main.rs
@@ -5,19 +5,19 @@ mod utils {
 }
 
 mod twenty_eighteen {
-    pub mod day02 {
-        pub mod day_two;
+    pub mod day03 {
+        pub mod day_three;
     }
 }
 
 use utils::file::read_file_as_vector;
-use crate::twenty_eighteen::day02::day_two::solve_two;
+use crate::twenty_eighteen::day03::day_three::solve;
 
 fn main() {
     let file_path = "C:/Users/justi/AdventOfCode/rust/src/twenty_eighteen/day02/input.txt";
 
     if let Ok(contents) = read_file_as_vector(file_path) {
-        match solve_two(&contents) {
+        match solve(&contents) {
             Ok(checksum) => println!("{}", checksum),
             Err(err) => println!("Oops something went wrong {:?}", err)
         }
