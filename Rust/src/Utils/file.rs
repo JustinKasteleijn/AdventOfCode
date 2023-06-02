587ea9bd7741431826a94071331015c5f89fdf26
JustinKasteleijn <48954608+JustinKasteleijn@users.noreply.github.com>
2023-06-02 20:49:57 +0200

    Finished day 2

--- Rust/src/Utils/file.rs
+++ Rust/src/Utils/file.rs
@@ -1,10 +1,12 @@
 use std::{fs, io};
 use std::io::BufRead;
 
+#[allow(dead_code)]
 pub fn read_file_as_string(file_path: &str) -> Result<String, io::Error> {
     return fs::read_to_string(file_path);
 }
 
+#[allow(dead_code)]
 pub fn read_file_as_vector(file_path: &str) -> Result<Vec<String>, io::Error> {
     return io::BufReader::new(
         fs::File::open(file_path)?
--- Rust/src/main.rs
+++ Rust/src/main.rs
@@ -1,21 +1,24 @@
+#![feature(let_chains)]
+
 mod utils {
     pub mod file;
 }
 
-mod day01 {
-    pub mod day_one;
+mod twenty_eighteen {
+    pub mod day02 {
+        pub mod day_two;
+    }
 }
 
-
 use utils::file::read_file_as_vector;
-use crate::day01::day_one::solve_two;
+use crate::twenty_eighteen::day02::day_two::solve_two;
 
 fn main() {
-    let file_path = "C:/Users/justi/AdventOfCode/rust/src/day01/input.txt";
+    let file_path = "C:/Users/justi/AdventOfCode/rust/src/twenty_eighteen/day02/input.txt";
 
     if let Ok(contents) = read_file_as_vector(file_path) {
-        match solve_two(contents) {
-            Ok(sum) => println!("{}", sum),
+        match solve_two(&contents) {
+            Ok(checksum) => println!("{}", checksum),
             Err(err) => println!("Oops something went wrong {:?}", err)
         }
     } else {
